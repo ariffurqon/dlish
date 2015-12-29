bfac371d9ff068643728d5ee6e625444555b0518
ariffurqon <arif.furqon@gmail.com>
2015-12-28 22:16:35 -0800

    added navigation

--- js/controllers/signupCtrl.js
+++ js/controllers/signupCtrl.js
@@ -1,6 +1,12 @@
 myApp.controller('signupCtrl', ['$scope', function($scope) {
 	$scope.login = function() {
-		$scope.message = "Welcome" + $scope.user.email;
+		$scope.message = "Welcome " + $scope.user.email;
 	}
+
+	$scope.signup = function() {
+		$scope.message = "Welcome " + $scope.user.firstname;
+	}
+
+
 }]);
 
