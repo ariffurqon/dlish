44ba71eb68f7758e5ea20fd346e68fa9ea233a7c
ariffurqon <arif.furqon@gmail.com>
2015-12-29 23:58:06 -0800

    adding records to the db

--- js/controllers/listingsCtrl.js
+++ js/controllers/listingsCtrl.js
@@ -1,3 +1,27 @@
-myApp.controller('listingsCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
-	
-}]);
+myApp.controller('listingsCtrl', 
+	['$scope', 
+	'$rootScope', 
+	'$firebaseAuth', 
+	'$firebaseArray', 
+	'FIREBASE_URL', 
+	function($scope, $rootScope, $firebaseAuth, $firebaseArray, FIREBASE_URL) {
+	var ref = new Firebase(FIREBASE_URL);
+	var auth = $firebaseAuth(ref);
+
+	auth.$onAuth(function(authUser){
+		if (authUser) {
+			var listingsRef = new Firebase(FIREBASE_URL + 'users/' + 
+				$rootScope.currentUser.$id + '/listings');
+			var listingsInfo = $firebaseArray(listingsRef);
+
+			$scope.addListing = function() {
+				listingsInfo.$add({
+					name: $scope.listingname,
+					date: Firebase.ServerValue.TIMESTAMP
+				}).then (function() {
+					$scope.listingname = '';
+				}); 
+			}; // add listing
+		} // user authenticated
+	});
+}]); // controller
