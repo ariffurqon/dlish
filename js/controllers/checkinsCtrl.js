22a37c7e94322d075883fce23d77aa6778bc4fb1
ariffurqon <arif.furqon@gmail.com>
2015-12-30 15:12:12 -0800

    added the check-in method

--- js/controllers/checkinsCtrl.js
+++ js/controllers/checkinsCtrl.js
@@ -6,6 +6,22 @@ myApp.controller('checkinsCtrl',
 	'$routeParams',
 	'FIREBASE_URL',
 	function($scope, $rootScope, $firebaseObject, $firebaseArray, $routeParams, FIREBASE_URL) {
- 		
- 		
+
+	$scope.whichlisting = $routeParams.lId;
+	$scope.whichuser = $routeParams.uId;
+
+	var ref = new Firebase(FIREBASE_URL + 'users/' + 
+		$scope.whichuser + '/listings/' +
+		$scope.whichlisting + '/checkins/');
+
+	$scope.addCheckin = function() {
+		var checkinsInfo = $firebaseArray(ref);
+		var myData = {
+			place: $scope.user.place,
+			activity: $scope.user.activity,
+			contact: $scope.user.contact,
+			date: Firebase.ServerValue.TIMESTAMP
+		}; //myData
+	checkinsInfo.$add(myData);
+	}; // addCheckin
  }]); // controller
